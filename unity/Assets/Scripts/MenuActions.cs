c467d1564c94c02959807b813d0f130a23cde8f5
maltaesousa <maltaesousa@gmail.com>
2019-07-26 11:28:24 +0200

    Menu Actions Script

---
+++ unity/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SpawnBuilding(int id)
+    {
+        print("BUILDING: " + id);
+    }
+}
