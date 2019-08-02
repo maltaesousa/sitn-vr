38eafa199df2b4102a86d8c5ed93d0de08efe44e
maltaesousa <maltaesousa@gmail.com>
2019-08-02 17:22:08 +0200

    Incremental moving forward of building is working

--- unity/Assets/Scripts/SitnPointer.cs
+++ unity/Assets/Scripts/SitnPointer.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Valve.VR.InteractionSystem;
 
 public class SitnPointer : MonoBehaviour
 {
     public float defaultLength = 0.5f;
     public GameObject endDot;
+    public Hand hand;
     public VRInputManager inputModule;
 
     private LineRenderer lineRenderer = null;
     private bool autoLength = true;
+    private float lastLength = 0.5f;
+
+    private float offset = 0.0f;
+
+    public void SetOffset(float value)
+    {
+        offset = value;
+    }
 
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        lastLength = defaultLength;
     }
 
     private void Update()
     {
-        UpdateLine();
+        UpdateLineState();
     }
 
     public void Show(bool value)
@@ -31,23 +42,53 @@ public class SitnPointer : MonoBehaviour
     {
         autoLength = value;
     }
-    
-    private void UpdateLine()
+
+    public GameObject GetAttachedObject()
     {
-        // Use default or provided distance
+        return hand.currentAttachedObject;
+    }
+    
+    private void UpdateLineState()
+    {
         PointerEventData data = inputModule.GetData();
-        float targetLength = data.pointerCurrentRaycast.distance == 0 ? defaultLength : data.pointerCurrentRaycast.distance;
+        float targetLength;
+        float inputRayLength = data.pointerCurrentRaycast.distance;
+        if (inputRayLength == 0)
+        {
+            targetLength = defaultLength;
+        } else
+        {
+            targetLength = inputRayLength;
+        }
 
         // Raycast
         RaycastHit hit = CreateRaycast(targetLength);
 
-        // Default dot
+        Vector3 endPosition;
 
-        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+        // Don't update en position until the attached object is released
+        if (GetAttachedObject() == null)
+        {
+            endPosition = transform.position + (transform.forward * targetLength);
+        }
+        else
+        {
+            endPosition = transform.position + (transform.forward * lastLength);
+        }
 
-        // Get end position if collider is hit
-        if (hit.collider != null && autoLength)
+        if (autoLength && hit.collider != null)
+        {   
             endPosition = hit.point;
+            lastLength = hit.distance;
+        }
+        else if (!autoLength)
+        {
+            // if there's an offset update lenght otherwise continue using a fixed length
+            targetLength = offset == 0.0f ? lastLength : lastLength + offset;
+            lastLength = targetLength;
+            endPosition = transform.position + (transform.forward * targetLength);
+            print("FIXED" + lastLength);
+        }
 
         // Update position of the dot
         endDot.transform.position = endPosition;
@@ -55,15 +96,20 @@ public class SitnPointer : MonoBehaviour
         // Update the line renderer
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, endPosition);
+        offset = 0.0f;
     }
 
     private RaycastHit CreateRaycast(float lenght)
     {
+        // collide against everything except layer 8.
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
         RaycastHit hit;
         // This creates a ray with two Vector3.
         Ray ray = new Ray(transform.position, transform.forward);
         // This creates the Raycast. the variable hit will contain information about what is hit
-        Physics.Raycast(ray, out hit, defaultLength);
+        Physics.Raycast(ray, out hit, lenght, layerMask);
 
         return hit;
     }
