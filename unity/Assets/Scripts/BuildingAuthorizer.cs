0a1ba4eaedd7dd1c1c2dcda51f82ec2954269680
maltaesousa <maltaesousa@gmail.com>
2019-08-04 21:56:00 +0200

    Building allowed places init

---
+++ unity/Assets/Scripts/BuildingAuthorizer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingAuthorizer : MonoBehaviour
+{
+    private BoxCollider bCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bCollider = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3[] groundBounds;
+        if (other.GetComponentInParent<PlayableBuilding>() != null)
+        {
+            PlayableBuilding building = other.GetComponentInParent<PlayableBuilding>();
+            groundBounds = building.GetColliderVertices();
+            bool isFullyContained = true;
+            for (int i = 0; i < groundBounds.Length; i++)
+            {
+                if (!bCollider.bounds.Contains(groundBounds[i]))
+                {
+                    isFullyContained = false;
+                }
+            }
+            building.SetValid(isFullyContained);
+        }
+    }
+}
