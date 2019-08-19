ff42fbae4872dbf947b7bb6a8e87e235863bfd04
maltaesousa <maltaesousa@gmail.com>
2019-08-19 19:03:05 +0200

    Validation working

--- unity/Assets/Scripts/PlayableBuilding.cs
+++ unity/Assets/Scripts/PlayableBuilding.cs
@@ -12,10 +12,10 @@ public class PlayableBuilding : MonoBehaviour
     {
         building = transform.GetChild(0).gameObject;
         BoxCollider bc = transform.GetComponent<BoxCollider>();
-        colliderVertices[0] = bc.center + new Vector3(bc.size.x, -bc.size.y, bc.size.z) * 0.5f;
-        colliderVertices[1] = bc.center + new Vector3(-bc.size.x, -bc.size.y, bc.size.z) * 0.5f;
-        colliderVertices[2] = bc.center + new Vector3(-bc.size.x, -bc.size.y, -bc.size.z) * 0.5f;
-        colliderVertices[3] = bc.center + new Vector3(bc.size.x, -bc.size.y, -bc.size.z) * 0.5f;
+        colliderVertices[0] = bc.transform.position + new Vector3(bc.size.x, -bc.size.y, bc.size.z) * 0.5f;
+        colliderVertices[1] = bc.transform.position + new Vector3(-bc.size.x, -bc.size.y, bc.size.z) * 0.5f;
+        colliderVertices[2] = bc.transform.position + new Vector3(-bc.size.x, -bc.size.y, -bc.size.z) * 0.5f;
+        colliderVertices[3] = bc.transform.position + new Vector3(bc.size.x, -bc.size.y, -bc.size.z) * 0.5f;
         return colliderVertices;
     }
 
